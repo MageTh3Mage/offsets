6f4f98e9e32af13a2bc24468e62be60429cd2eb8
Mage <connaroegarrett@gmail.com>
2025-12-24 14:25:49 -0600

    Refactor offsets in offsets.h for clarity

--- offsets.h
+++ offsets.h
@@ -5,7 +5,6 @@
   "LocalPlayers": "0x38",
   "PlayerController": "0x30",
   "LocalPawn": "0x358",
-
   "PlayerArray": "0x2C8",
   "Private": "0x328",
   "PlayerState": "0x2D0",
@@ -14,24 +13,19 @@
   "bIsDBNO": "0x841",
   "bIsDying": "0x728",
   "bIsInvulnerable": "0x72A",
-
   "RootComponent": "0x1B0",
   "Mesh": "0x330",
   "ComponentToWorld": "0x1E0",
   "BoneArray": "0x5F0",
   "BoneArrayCache": "0x600",
   "Velocity": "0xD8",
-
   "RelativeLocation": "0x140",
   "RelativeRotation": "0x158",
-
   "CameraLocation": "0x178",
   "CameraRotation": "0x188",
   "FOV": "0x3B4",
   "WorldTimeSeconds": "0x198",
-
   "LastRenderTimeOnScreen": "0x328",
-
   "CurrentWeapon": "0x990",
   "WeaponData": "0x698",
   "Rarity": "0xAA",
@@ -40,20 +34,14 @@
   "FLength": "0x28",
   "ProjectileSpeed": "0x24F4",
   "ProjectileGravity": "0x24F8",
-
   "TargetedFortPawn": "0x1840",
-
   "EncryptedPlayerNameStruct": "0xA00",
-
   "Platform": "0x440",
-
   "Spectators": "0xA98",
   "SpectatorArray": "0x108",
-
   "KillScore": "0x11C0",
   "RankedProgress": "0xD8",
   "HabaneroComponent": "0x940",
-
   "bAlreadySearched": "0xD52",
   "bPickedUp": "0x2CC",
   "HighestRarity": "0xD50",
